2ab907da835519267b8cc3c6515ea805dc5f7c12
f4rral <f4rral@gmail.com>
2024-10-30 22:18:45 +0300

    Header, TemperatureText

--- app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -3,23 +3,55 @@ package com.hfad.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                Hello("friend")
-                Hello("everyone")
-            }
+            MainActivityContent()
         }
     }
 }
 
 @Composable
-fun Hello(name: String) {
-    Text("Hello $name! Welcome to Jetpack Compose")
+fun Header(image: Int, description: String) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = description,
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun TemperatureText(celsius: Int) {
+    val fahrenheit = (celsius.toDouble() * 9 / 5) + 32
+    Text("$celsius Celsius is $fahrenheit Fahrenheit")
+}
+
+@Composable
+fun MainActivityContent() {
+    Column {
+        Header(R.drawable.sunrise, "sunrise image")
+        TemperatureText(0)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMainActivity() {
+    MainActivityContent()
 }
