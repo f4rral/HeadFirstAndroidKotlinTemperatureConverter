9cc48cba738708c57e089cf32a283d6c55d6a7b7
f4rral <f4rral@gmail.com>
2024-10-30 21:22:12 +0300

    Text

--- app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -3,10 +3,23 @@ package com.hfad.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+        setContent {
+            Column {
+                Hello("friend")
+                Hello("everyone")
+            }
+        }
     }
+}
+
+@Composable
+fun Hello(name: String) {
+    Text("Hello $name! Welcome to Jetpack Compose")
 }
