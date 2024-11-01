1e0dd0ba984071596d9bab143cfbe86022043a55
f4rral <f4rral@gmail.com>
2024-11-01 21:26:26 +0300

    Theme

--- app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,12 +24,17 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hfad.temperatureconverter.ui.theme.TemperatureConverterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityContent()
+            TemperatureConverterTheme {
+                Surface {
+                    MainActivityContent()
+                }
+            }
         }
     }
 }
@@ -71,12 +81,21 @@ fun MainActivityContent() {
     val celsius = remember { mutableIntStateOf(0) }
     val newCelsius = remember { mutableStateOf("") }
 
-    Column {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
         Header(R.drawable.sunrise, "sunrise image")
         EnterTemperature(newCelsius.value) { newCelsius.value = it }
-        ConvertButton {
-            newCelsius.value.toIntOrNull()?.let {
-                celsius.intValue = it
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            ConvertButton {
+                newCelsius.value.toIntOrNull()?.let {
+                    celsius.intValue = it
+                }
             }
         }
         TemperatureText(celsius.intValue)
@@ -86,5 +105,9 @@ fun MainActivityContent() {
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainActivity() {
-    MainActivityContent()
+    TemperatureConverterTheme {
+        Surface {
+            MainActivityContent()
+        }
+    }
 }
